de73484e45ed9a57ae9c4e140513304ced521759
Erik Pujat <eriks.pujats@gmail.com>
2022-05-17 23:21:20 +0300

    Still just adding java file

--- HomeworkThing.java
+++ HomeworkThing.java
@@ -1,49 +1,45 @@
-package test;
+package test; //Can be deleted, if not intended to stay within original package.
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class HomeworkThing {
-	
-	private static String oogabooga = "Onga Bonga";
+	public static final int VARIABLE_X = 4;
+	public static final int VARIABLE_Y = 5;
+	public static final int VARIABLE_Z = 11;
+	public static final int RESULT_VARIABLE = 44;
 
 	@Test
-	void test_counter() {
-		System.out.println("Counting characters in string \"Hello world\" (including space)... ");
-		HomeworkThing t_variable = new HomeworkThing();
-		assertTrue(t_variable.countCharacters("Hello world") == 11);
+	void testCounter() {
+		System.out.println("Counting characters in string "
+			+ "\"Hello world\" (including space)... ");
+		HomeworkThing tVariable = new HomeworkThing();
+		assertTrue(tVariable.countCharacters("Hello world") == VARIABLE_Z);
 		System.out.println("Everything's alright?");
 	}
-	
 	@Test
-	public void test_multiplication() {
-		System.out.println("Multiplication between 11 and 4. Expected value is 44");
-		HomeworkThing t_variable = new HomeworkThing();
-		assertTrue(t_variable.multiply(11, 4) == 44);
+	public void testMultiplication() {
+		System.out.println("Multiplication between 11 and 4." 
+			+ " Expected value is 44");
+		HomeworkThing tVariable = new HomeworkThing();
+		assertTrue(tVariable.multiply(VARIABLE_Z, VARIABLE_X) == RESULT_VARIABLE);
 		System.out.println("Everything's alright?");
 	}
-	public int multiply(int x, int y) {
-		return x*y;
+	public static int multiply(int x, int y) { //Multiplies two values
+		return x * y;
 	}
-	
-	public int countCharacters(String word) {
+	public static int countCharacters(String word) { //Counts caracters in a given string
 		int count = 0;
-		for(int i = 0; i < word.length(); i++) {
+		for (int i = 0; i < word.length(); i++) {
 			count++;
 		}
 		return count;
 	}
-	
-	void printooga() {
-		System.out.println(oogabooga);
-	}
-	
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Heyo\n");
-		HomeworkThing Lmao = new HomeworkThing();
-		System.out.println("Multiplying 4 * 5 = " + Lmao.multiply(4, 5));
-		System.out.println("\nCounting characters in \"Homework is done\".\nResult = " + Lmao.countCharacters("Homework is done") + " characters");
+		HomeworkThing lmaoMul = new HomeworkThing();
+		System.out.println("Multiplying 4 * 5 = "
+			+ lmaoMul.multiply(VARIABLE_X, VARIABLE_Y));
+		System.out.println("\nCounting characters in \"Homework is done\".\nResult = " 
+			+ lmaoMul.countCharacters("Homework is done") + " characters");
 	}
-	
-	
-	
 }
