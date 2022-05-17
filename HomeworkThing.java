5d3b6452eb677ad645099f92b994790574c364ba
Erik Pujat <eriks.pujats@gmail.com>
2022-05-17 22:36:24 +0300

    Still just adding java file

--- HomeworkThing.java
+++ HomeworkThing.java
@@ -1,12 +1,26 @@
 package test;
-
-
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 public class HomeworkThing {
 	
-	static String oogabooga = "Onga Bonga";
+	private static String oogabooga = "Onga Bonga";
+
+	@Test
+	void test_counter() {
+		System.out.println("Counting characters in string \"Hello world\" (including space)... ");
+		HomeworkThing t_variable = new HomeworkThing();
+		assertTrue(t_variable.countCharacters("Hello world") == 11);
+		System.out.println("Everything's alright?");
+	}
 	
-	
+	@Test
+	public void test_multiplication() {
+		System.out.println("Multiplication between 11 and 4. Expected value is 44");
+		HomeworkThing t_variable = new HomeworkThing();
+		assertTrue(t_variable.multiply(11, 4) == 44);
+		System.out.println("Everything's alright?");
+	}
 	public int multiply(int x, int y) {
 		return x*y;
 	}
@@ -29,4 +43,7 @@ public class HomeworkThing {
 		System.out.println("Multiplying 4 * 5 = " + Lmao.multiply(4, 5));
 		System.out.println("\nCounting characters in \"Homework is done\".\nResult = " + Lmao.countCharacters("Homework is done") + " characters");
 	}
+	
+	
+	
 }
